82d7f0f3c8fefaf1851e7dc2dbab7f41692b9987
Carlos Maldonado <kardaniel1998@gmail.com>
2021-03-03 11:35:01 -0500

    Cambiar el formulario de quejas a componente

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -1,6 +1,78 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import {Controller, useForm} from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import {Complaint} from "@/lib/complaints";
 
-export default function FormSection() {
+const  FormSection = ()=> {
 
-    return("");
+    const {handleSubmit, control} = useForm();
+
+    const onSubmit = async (data) => {
+        console.log("data", data);
+        try {
+            const complaintData = await Complaint.sendComplaint(data);
+            console.log('Queja enviada: ', complaintData);
+        } catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                alert(error.response.message);
+                console.log(error.response.data);
+                //d
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        }
+    };
+
+
+    return(
+        <>
+            <CssBaseline/>
+            <Grid container component="main">
+                <Grid item xs={12} elevation={2}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <FormControl fullWidth variant="outlined">
+                            <Controller
+                                name="email"
+                                as={
+                                    <TextField
+                                        id="email"
+                                        labelWidth={40}
+                                        variant="outlined"
+                                        label="Email"
+                                    />
+                                }
+                                control={control}
+                                defaultValue=""
+                                rules={{
+                                    required: 'Required',
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: 'invalid email address'
+                                    }
+                                }}
+                            />
+                        </FormControl>
+                        <Button variant="contained" color="primary" type="submit">
+                            ENVIAR
+                        </Button>
+                    </form>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
+
+export default FormSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,13 @@
 import React from "react";
-import {Complaint} from "@/lib/complaints";
-import {Controller, useForm} from "react-hook-form";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Button from "@material-ui/core/Button";
-import FormControl from "@material-ui/core/FormControl";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
+import FormSection from "@/components/FormSection";
 
 export default function Home() {
-    const {handleSubmit, control} = useForm();
 
-    const onSubmit = async (data) => {
-        console.log("data", data);
-        try {
-            const complaintData = await Complaint.sendComplaint(data);
-            console.log('Queja enviada: ', complaintData);
-        } catch (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                alert(error.response.message);
-                console.log(error.response.data);
-                //d
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-            }
-            console.log(error.config);
-        }
-    };
     return (
         <>
-            <CssBaseline/>
-            <Grid container component="main">
-                <Grid item xs={12} elevation={2}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <FormControl fullWidth variant="outlined">
-                            <Controller
-                                name="email"
-                                as={
-                                    <TextField
-                                        id="email"
-                                        labelWidth={40}
-                                        variant="outlined"
-                                        label="Email"
-                                    />
-                                }
-                                control={control}
-                                defaultValue=""
-                                rules={{
-                                    required: 'Required',
-                                    pattern: {
-                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                                        message: 'invalid email address'
-                                    }
-                                }}
-                            />
-                        </FormControl>
-                        <Button variant="contained" color="primary" type="submit">
-                            ENVIAR
-                        </Button>
-                    </form>
-                </Grid>
-            </Grid>
+            {/*<InfoSection />*/}
+            {/*<OptionSection />*/}
+            <FormSection />
         </>
     );
 }
