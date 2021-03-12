ae5be5e3870c0687aaba4a71bc4b68465dcaf92b
Carlos Maldonado <kardaniel1998@gmail.com>
2021-03-12 13:58:40 -0500

    Cambiar el select

--- src/pages/barrios/index.js
+++ src/pages/barrios/index.js
@@ -4,14 +4,14 @@ import {fetcher} from "@/lib/utils";
 import Loading from "../../components/Loading";
 import FormControl from "@material-ui/core/FormControl";
 import {
-    Box, Divider,
-    InputBase,
+    Box, Divider, FormHelperText,
+    InputBase, InputLabel,
     List,
     ListItem,
     ListItemAvatar,
     ListItemText,
     makeStyles,
-    MenuItem,
+    MenuItem, NativeSelect,
     Select, Typography, withStyles
 } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
@@ -34,7 +34,7 @@ const BootstrapInput = withStyles(theme => ({
         backgroundColor: theme.palette.background.paper,
         border: '1px solid #ced4da',
         fontSize: 16,
-        width: 200,
+        width: '250',
         padding: '10px 26px 10px 12px',
         transition: theme.transitions.create(['border-color', 'box-shadow']),
 
@@ -83,6 +83,10 @@ const useStyles = makeStyles((theme) => ({
     inline: {
         display: 'inline',
     },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
 }));
 
 
@@ -90,7 +94,10 @@ const Neighborhoods = () => {
 
     const classes = useStyles();
     const {data, error} = useSWR(`/neighborhoods/all`, fetcher);
-    const [name, setName] = useState("");
+    const [name, setName] = useState({
+        id: "",
+        neighborhood: ""
+    });
     const [neighborhoodID, setNeighborhoodID] = useState([{
         id: "",
         name: "",
@@ -104,16 +111,23 @@ const Neighborhoods = () => {
     if (!data) return <Loading/>;
 
     const handleChange = event => {
-        setName(event.target.value);
-        setNeighborhoodID({
-            ...neighborhoodID,
-            id: event.target.value.id,
-            name: event.target.value.name,
-            start_time: event.target.value.start_time,
-            end_time: event.target.value.end_time,
-            days: event.target.value.days,
-            link: event.target.value.link
+        const number = event.target.value;
+        setName({
+            ...name,
+            id: number ? number : "",
+            neighborhood: number ? data.data[number].name : "",
         });
+        number ?
+            setNeighborhoodID({
+                ...neighborhoodID,
+                id: data.data[number].id,
+                name: data.data[number].name,
+                start_time: data.data[number].start_time,
+                end_time: data.data[number].end_time,
+                days: data.data[number].days,
+                link: data.data[number].link
+            })
+            : setNeighborhoodID("");
     };
 
 
@@ -127,33 +141,30 @@ const Neighborhoods = () => {
                     <p>Seleccione el barrio que desee consultar sus horarios</p>
                 </div>
 
-                <form className={classes.root} autoComplete="off">
-                    <FormControl className={classes.margin}>
-                        <Select
-                            value={name}
-                            onChange={handleChange}
-                            input={<BootstrapInput name="neighborhood" id="age-customized-select"/>}
-                        >
-                            <MenuItem value="">
-                                Seleccione el Barrio
-                            </MenuItem>
-                            {
-                                data.data.map((neighborhood) => {
-                                        return (
-                                            <MenuItem value={neighborhood} key={neighborhood.id}>
-                                                {neighborhood.name}
-                                            </MenuItem>
-                                        )
-                                    }
-                                )
-                            }
-                        </Select>
-                    </FormControl>
-                </form>
-
+                <FormControl variant="outlined" className={classes.formControl}>
+                    <InputLabel htmlFor="outlined-age-native-simple">Barrio</InputLabel>
+                    <Select
+                        native
+                        value={name.id}
+                        onChange={handleChange}
+                        label="Barrio"
+                    >
+                        <option aria-label="None" value=""/>
+                        {
+                            data.data.map((neighborhood, index) => {
+                                    return (
+                                        <option value={index}>
+                                            {neighborhood.name}
+                                        </option>
+                                    )
+                                }
+                            )
+                        }
+                    </Select>
+                </FormControl>
 
                 {
-                    name ?
+                    name.neighborhood !== "" ?
                         <div>
                             <List className={classes.root2}>
                                 <ListItem alignItems="flex-start">
@@ -230,6 +241,7 @@ const Neighborhoods = () => {
                             </Box>
                         </div>
                 }
+
 
                 <div>
                     <Box display="flex" justifyContent="center" m={1} p={1}>
