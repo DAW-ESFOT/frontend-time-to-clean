eb08c8be88b13be7d978edc5f7800bf4381b2797
Brenda Arcentales <brenda.arcentales@epn.edu.ec>
2021-03-17 09:18:23 -0500

    Correcciones

--- src/pages/acerca-de.js
+++ src/pages/acerca-de.js
@@ -25,7 +25,7 @@ const styles = {
 const AcercaDe = () => {
     return (
         <div style={styles.Container}>
-            <Typography variant="h4" gutterBottom style={styles.Title}>¿Quienes somos?</Typography>
+            <Typography variant="h4" gutterBottom style={styles.Title}>¿Quiénes somos?</Typography>
             <Grid container direction="row" justify="space-around" alignItems="center">
                 <Grid item xs={12} sm={3}>
                     <Image src="/arrow.png" width={100} height={150}/>
@@ -33,10 +33,10 @@ const AcercaDe = () => {
                 <Grid item xs={12} sm={6}>
                     <Paper style={styles.Paper} elevation={3}>
                         <Typography variant="body1" gutterBottom>
-                            Time2Clean nace como una idea de proyecto de Desarrollo de Aplicaciones Web en el año 2020,
+                            Time To Clean nace como una idea de proyecto de Desarrollo de Aplicaciones Web en el año 2020,
                             creada por Brenda, Carlos, Eddy y Gabriela, estudiantes de la Escuela de Formación de
                             Tecnólogos de la EPN.<br/> La información que proporciona esta plataforma se presenta de
-                            manera intutitva para que la gestion y comunicacion entre personal del servicio y
+                            manera intuitiva para que la gestión y comunicación entre personal del servicio y
                             ciudadanos sea fluida y actualizada.
                         </Typography>
                     </Paper>
@@ -62,7 +62,7 @@ const AcercaDe = () => {
                     <Paper style={styles.Paper} elevation={3}>
                         <Typography variant="h4" gutterBottom>Visión</Typography>
                         <Typography variant="body1" gutterBottom>
-                            Para el 2025, Time to clean será reconocida por facilitar el control del servicio de aseo
+                            Para el 2025, Time To Clean será reconocida por facilitar el control del servicio de aseo
                             en Quito, aplicando métodos innovadores que integren a cada ciudadano para reducir la
                             cantidad de residuos mediante la promoción de la responsabilidad individual y corporativa.
                         </Typography>
