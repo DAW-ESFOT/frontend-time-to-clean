8c367f8fc0b1ca7a889f068760e4420b337c7f08
Carlos Maldonado <kardaniel1998@gmail.com>
2021-03-16 22:38:44 -0500

    Filtro de quejas

--- src/components/TableComplaints.js
+++ src/components/TableComplaints.js
@@ -4,7 +4,7 @@ import {fetcher} from "@/lib/utils";
 import Loading from "@/components/Loading";
 import withAuth from "@/hocs/withAuth";
 import {withStyles} from "@material-ui/core/styles";
-import {Dialog, DialogContent} from '@material-ui/core';
+import {Box, Dialog, DialogContent, FormControlLabel, FormLabel, Grid, Radio, RadioGroup} from '@material-ui/core';
 import {
     Paper,
     TableRow,
@@ -18,6 +18,7 @@ import {
 } from "@material-ui/core";
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import EditComplaint from "@/components/EditComplaint";
+import FormControl from "@material-ui/core/FormControl";
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -37,25 +38,31 @@ const StyledTableRow = withStyles((theme) => ({
     },
 }))(TableRow);
 
-const styles={
-    title:{
-        textAlign:'center',
-        color:'white',
+const styles = {
+    title: {
+        textAlign: 'center',
+        color: 'white',
         textShadow: '2px 2px #262626',
-    }
+    },
+    Paper: {
+        backgroundColor: 'rgba(255,255,255)',
+        margin: '5px',
+        padding: '5px',
+    },
 };
 
 const TableComplaints = () => {
     const [page, setPage] = useState(0);
-    const {data, error} = useSWR(`/complaints?page=${page + 1}`, fetcher);
+    const [filter, setFilter] = useState("");
+    const {data, error, mutate} = useSWR(`/complaints?page=${page + 1}`, fetcher);
+    const {data: dataFilter, error: errorFilter, mutate: mutateFilter} = useSWR(`/complaints${filter}`, fetcher);
+
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [ComplaintId, setComplaintId] = useState(0);
     const [openEditComplaint, setOpenEditComplaint] = useState(false);
 
     console.log("Complaints", data);
 
-    if (error) return <div>No se pudieron cargar las quejas</div>;
-    if (!data) return <Loading/>;
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -66,74 +73,166 @@ const TableComplaints = () => {
         setComplaintId(id);
     };
     const handleCloseEditComplaint = () => {
-        setOpenEditComplaint(!openEditComplaint);
+        setOpenEditComplaint( false );
+        mutate();
+        mutateFilter();
     };
 
+    const handleChange = (event) => {
+        setFilter(event.target.value);
+        console.log(filter);
+    }
+
+    if (error) return <div>No se pudieron cargar las quejas</div>;
+    if (!data) return <Loading/>;
+
+    console.log("filtro", dataFilter);
     return (
         <>
             <h1 style={styles.title}> Gestión de quejas</h1>
+
+            <FormControl component="fieldset">
+                <Paper style={styles.Paper} elevation={0}>
+                    <FormLabel component="legend">Filtrar Estado </FormLabel>
+                    <RadioGroup aria-label="gender" value={filter} onChange={handleChange}>
+                        <Box display="flex" justifyContent="center" m={1} p={1}>
+                            <FormControlLabel value={""} control={<Radio/>} label="Todas"/>
+                            <FormControlLabel value={"/filter/state1"} control={<Radio/>} label="Pendiente"/>
+                            <FormControlLabel value={"/filter/state2"} control={<Radio/>} label="Proceso"/>
+                            <FormControlLabel value={"/filter/state3"} control={<Radio/>} label="Atendida"/>
+                        </Box>
+                    </RadioGroup>
+                </Paper>
+            </FormControl>
+
+
             <div>
                 {
-                    data ?
-                        <div>
-                            <TableContainer component={Paper}>
-                                <Table aria-label="customized table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <StyledTableCell align="center" style={{ width: 100 }}>Fecha</StyledTableCell>
-                                            <StyledTableCell align="center" style={{ width: 350 }}>Queja</StyledTableCell>
-                                            <StyledTableCell align="center">Información</StyledTableCell>
-                                            <StyledTableCell align="center">Estado</StyledTableCell>
-                                            <StyledTableCell align="center" style={{ width: 200 }}>Observación</StyledTableCell>
-                                            <StyledTableCell align="center"></StyledTableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {data.data.map((Complaint) => (
-                                            <StyledTableRow key={Complaint.id}>
-                                                <StyledTableCell align="center">
-                                                    {(Complaint.created_at).substr(0, 10)}<br/>
-                                                    {(Complaint.created_at).substr(11, 5)}
-                                                </StyledTableCell>
-                                                <StyledTableCell align="justify">
-                                                    {(Complaint.complaint).substr(0, 135)} [...]
-                                                </StyledTableCell>
-                                                <StyledTableCell align="left">
-                                                    Barrio: {Complaint.neighborhood_id}<br/>
-                                                    Camión: {Complaint.truck.license_plate}<br/>
-                                                    Conductor: {Complaint.truck.user.name} {Complaint.truck.user.lastname}<br/>
-                                                </StyledTableCell>
-                                                <StyledTableCell align="center">
-                                                    {Complaint.state}
-                                                </StyledTableCell>
-                                                <StyledTableCell align="justify">
-                                                    {Complaint.observation}
-                                                </StyledTableCell>
-                                                <StyledTableCell align="center">
-                                                    <IconButton
-                                                        onClick={() => handleOpenEditComplaint(Complaint.id)}
-                                                        color="secondary"
-                                                        aria-label="upload picture"
-                                                        component="span">
-                                                        <BorderColorIcon/>
-                                                    </IconButton>
-                                                </StyledTableCell>
-                                            </StyledTableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                            <TablePagination
-                                rowsPerPageOptions={[10]}
-                                component="div"
-                                count={data.meta.total}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onChangePage={handleChangePage}
-                            />
-                        </div>
-                        :
-                        <Loading/>
+                    filter === "" ? (
+                        data ?
+                            <div>
+                                <TableContainer component={Paper}>
+                                    <Table aria-label="customized table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <StyledTableCell align="center"
+                                                                 style={{width: 100}}>Fecha</StyledTableCell>
+                                                <StyledTableCell align="center"
+                                                                 style={{width: 350}}>Queja</StyledTableCell>
+                                                <StyledTableCell align="center">Información</StyledTableCell>
+                                                <StyledTableCell align="center">Estado</StyledTableCell>
+                                                <StyledTableCell align="center"
+                                                                 style={{width: 200}}>Observación</StyledTableCell>
+                                                <StyledTableCell align="center"></StyledTableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {data.data.map((Complaint) => (
+                                                <StyledTableRow key={Complaint.id}>
+                                                    <StyledTableCell align="center">
+                                                        {(Complaint.created_at).substr(0, 10)}<br/>
+                                                        {(Complaint.created_at).substr(11, 5)}
+                                                    </StyledTableCell>
+                                                    <StyledTableCell align="justify">
+                                                        {(Complaint.complaint).substr(0, 135)} [...]
+                                                    </StyledTableCell>
+                                                    <StyledTableCell align="left">
+                                                        Barrio: {Complaint.neighborhood_name}<br/>
+                                                        Camión: {Complaint.truck.license_plate}<br/>
+                                                        Conductor: {Complaint.truck.user.name} {Complaint.truck.user.lastname}<br/>
+                                                    </StyledTableCell>
+                                                    <StyledTableCell align="center">
+                                                        {Complaint.state}
+                                                    </StyledTableCell>
+                                                    <StyledTableCell align="justify">
+                                                        {Complaint.observation}
+                                                    </StyledTableCell>
+                                                    <StyledTableCell align="center">
+                                                        <IconButton
+                                                            onClick={() => handleOpenEditComplaint(Complaint.id)}
+                                                            color="secondary"
+                                                            aria-label="upload picture"
+                                                            component="span">
+                                                            <BorderColorIcon/>
+                                                        </IconButton>
+                                                    </StyledTableCell>
+                                                </StyledTableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                                <TablePagination
+                                    rowsPerPageOptions={[10]}
+                                    component="div"
+                                    count={data.meta.total}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    onChangePage={handleChangePage}
+                                />
+                            </div>
+                            :
+                            <Loading/>
+                    ) : (
+                        dataFilter ?
+                            <div>
+                                <TableContainer component={Paper}>
+                                    <Table aria-label="customized table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <StyledTableCell align="center"
+                                                                 style={{width: 100}}>Fecha</StyledTableCell>
+                                                <StyledTableCell align="center"
+                                                                 style={{width: 350}}>Queja</StyledTableCell>
+                                                <StyledTableCell align="center">Información</StyledTableCell>
+                                                <StyledTableCell align="center">Estado</StyledTableCell>
+                                                <StyledTableCell align="center"
+                                                                 style={{width: 200}}>Observación</StyledTableCell>
+                                                <StyledTableCell align="center"></StyledTableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {dataFilter.data.map((Complaint) => (
+                                                Complaint.id ?
+                                                    <StyledTableRow key={Complaint.id}>
+                                                        <StyledTableCell align="center">
+                                                            {(Complaint.created_at).substr(0, 10)}<br/>
+                                                            {(Complaint.created_at).substr(11, 5)}
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="justify">
+                                                            {(Complaint.complaint).substr(0, 135)} [...]
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="left">
+                                                            Barrio: {Complaint.neighborhood_name}<br/>
+                                                            Camión: {Complaint.truck.license_plate}<br/>
+                                                            Conductor: {Complaint.truck.user.name} {Complaint.truck.user.lastname}<br/>
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="center">
+                                                            {Complaint.state}
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="justify">
+                                                            {Complaint.observation}
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="center">
+                                                            <IconButton
+                                                                onClick={() => handleOpenEditComplaint(Complaint.id)}
+                                                                color="secondary"
+                                                                aria-label="upload picture"
+                                                                component="span">
+                                                                <BorderColorIcon/>
+                                                            </IconButton>
+                                                        </StyledTableCell>
+                                                    </StyledTableRow>
+                                                    : ""
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </div>
+                            :
+                            <Loading/>
+                    )
+
+
                 }
                 <div>
                     <Dialog onClose={handleCloseEditComplaint} open={openEditComplaint}>
